f1e2c3ecd67bcc0abba9829b82e114d9ed81e494
Jamie Quigley <jamie@quigley.xyz>
2022-03-11 14:05:41 +0000

    Basic power monitor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::{thread, time::Duration};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut smc = macsmc::Smc::connect()?;
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+        let w = smc.power_system_total()?;
+        println!("Current power draw: {:.2}W", *w)
+    }
 }
